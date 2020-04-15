2528685902d19fbdc8087a83cff3116d94ecd099
KossySteve <kossyeze@gmail.com>
2020-04-15 19:27:05 +0100

    edited map methods

--- enumerable_methods.rb.rb
+++ enumerable_methods.rb.rb
@@ -1,6 +1,4 @@
-#module Enumerable
-
-#end
+module Enumerable
 public
 def my_each
     i = 0
@@ -12,7 +10,7 @@ def my_each
   self
 end
 
-#[2,3,41,2,24].my_each {|num| print num * 2}
+
 def my_each_with_index
       i = 0
       n = self.size
@@ -22,8 +20,6 @@ def my_each_with_index
     end
     self
 end
-
-#[2,3,41,2,24].my_each_with_index {|num, index| print index}
 
 
 def my_select
@@ -36,8 +32,6 @@ def my_select
     return my_select_array
 end
 
-#puts ""
-#print [1,2,3,4,5,6,7,8,100].my_select {|num| num%2 == 0}
 
 def my_all?
     i = 0
@@ -51,7 +45,8 @@ def my_all?
   end
   print all
 end
-#puts [2,4,8,2].my_all? {|num| num % 2 != 0}
+
+
 def my_any?
     i = 0
     any_item = false
@@ -64,7 +59,7 @@ def my_any?
   end
   print any_item
 end
-#puts [1,3,2,5,7]. my_any?{|x| x%2 != 0}
+
 
 def my_none?
     i = 0
@@ -78,7 +73,8 @@ def my_none?
   end
   print no_item
 end
-#puts [1,3,5,7].my_none?{|x| x%2 != 0}
+
+
 def my_count
   i = 0
   self.my_each {
@@ -89,7 +85,7 @@ def my_count
   }
   print i
 end
-#puts [1,2,3,4,5,7,2].my_count{ |x| x%2 == 0 }
+
 
 def my_inject
   i = 0
@@ -103,47 +99,48 @@ def my_inject
     injected
 end
 
+
 def multiply_els(array)
   array.my_inject {|total, item| total * item}
 end
-#puts multiply_els([2,4,5])
+
 
 
 def my_map
     my_map_array = []
     for item in self
-    item =  yield (item)
+    item =  yield(item)
     my_map_array << item
     end
     return my_map_array
 end
-#print [1,3,100,5,7,2].my_map {|num| num * 2}
 
 
-def my_map_with_proc
+def my_map(&proc)
     my_map_array = []
     for item in self
-    item =  my_proc.call
+    item =  proc.call(item)
     my_map_array << item
     end
     return my_map_array
 end
-my_proc = Proc.new {|num| num * 2}
-#print [1,3,100,5,7,2].my_map(&my_proc)
 
 
-def my_map_with_proc_or_block
+def my_map(&proc)
     my_map_array = []
-    for item in self
-      if block_given?
+    if block_given?
+        for item in self
         item =  yield (item)
         my_map_array << item
-      else
-        item =  my_proc.call
+        end
+    else
+        for item in self
+        item =  proc.call(item)
         my_map_array << item
-      end
+        end
+    end
     return my_map_array
-  end
 end
-my_proc = Proc.new {|num| num * 2}
-#print [1,3,100,5,7,2].my_map(&my_proc)
+
+
+end
