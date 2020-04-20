bdfd8f17b8a17c0c56d87b50b80ed364139ffa05
KossySteve <kossyeze@gmail.com>
2020-04-20 13:52:13 +0100

    fixed stickler errors

--- enumerable_methods1.rb
+++ enumerable_methods1.rb
@@ -97,12 +97,10 @@ module Enumerable
 
   def my_inject(initial = 0, opr = nil)
     my_array = []
+    obj = self
     if self.class == Range
-      obj = self
       obj.each { |i| my_array << i }
       obj = my_array
-    else
-      obj = self
     end
     if initial.class == (Symbol or String)
       opr = initial.to_s
