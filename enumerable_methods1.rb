a3bc5f1843ab66e3dd46dc76cb6b2c20adf48b69
KossySteve <kossyeze@gmail.com>
2020-04-22 01:37:56 +0100

    fixed linter error

--- enumerable_methods1.rb
+++ enumerable_methods1.rb
@@ -1,7 +1,6 @@
 # rubocop:disable Style/CaseEquality
 # rubocop:disable Metrics/CyclomaticComplexity
 # rubocop:disable Metrics/PerceivedComplexity
-# rubocop:disable Metrics/MethodLength:
 # rubocop:disable Metrics/ModuleLength
 
 # :nodoc:
@@ -140,5 +139,4 @@ end
 # rubocop:enable Style/CaseEquality
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/PerceivedComplexity
-# rubocop:enable Metrics/MethodLength:
 # rubocop:enable Metrics/ModuleLength
